1d7d1018bc6485d26632d441a9e581566ac3dac1
Erli Jacob <erli.balbinot@hotmail.com>
2025-10-31 16:36:47 -0300

    Ajustes

--- backend/src/main/java/com/jacob/backend/controller/CalculatorController.java
+++ backend/src/main/java/com/jacob/backend/controller/CalculatorController.java
@@ -3,12 +3,14 @@ package com.jacob.backend.controller;
 import com.jacob.backend.dto.InstallmentsDto;
 import com.jacob.backend.dto.RequestParams;
 import com.jacob.backend.service.CalculatorService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api")
@@ -21,7 +23,12 @@ public class CalculatorController {
     }
 
     @PostMapping("/calculate")
-    public ResponseEntity<InstallmentsDto> calculate(@RequestBody RequestParams params) {
-        return ResponseEntity.status(HttpStatus.OK).body(service.calculate(params));
+    public ResponseEntity<InstallmentsDto> calculate(@Valid @RequestBody RequestParams params) {
+        String dateValid = params.validateDates();
+        if (dateValid.equals("ok")) {
+            return ResponseEntity.status(HttpStatus.OK).body(service.calculate(params));
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, dateValid);
+        }
     }
 }
--- backend/src/main/java/com/jacob/backend/dto/RequestParams.java
+++ backend/src/main/java/com/jacob/backend/dto/RequestParams.java
@@ -1,5 +1,6 @@
 package com.jacob.backend.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,10 +16,27 @@ import java.time.LocalDate;
 @Builder
 public class RequestParams {
 
+    @NotNull(message = "A data inicial não pode ser nula")
     private LocalDate initDate;
+    @NotNull(message = "A data final não pode ser nula")
     private LocalDate finishDate;
+    @NotNull(message = "A data do pagamento incial não pode ser nula")
     private LocalDate initPayment;
+    @NotNull(message = "O valor do empréstimo não pode ser nulo")
     private Double loan;
+    @NotNull(message = "O valor do juros não pode ser nulo")
     private Double percent;
+
+    public String validateDates() {
+        if (this.finishDate.isBefore(this.initDate) &&
+            this.finishDate.isEqual(this.initDate)){
+            return "Verifique as datas! A data final deve ser maior que a data inicial.";
+        }
+        if (this.initPayment.isBefore(this.initDate) ||
+                this.initPayment.isAfter(this.finishDate)) {
+            return "Verifique as datas! O primeiro pagamento deve estar entre a data inicial e final.";
+        }
+        return "OK";
+    }
 
 }
